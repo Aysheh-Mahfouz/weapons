04d9a8ebf1167b4ac5ff4abfc0431cf68c294e70
aysheh-ayoosh <ayoosh.dub@hotmail.com>
2021-03-25 16:41:18 +0200

    submet with js

---
+++ app.js
@@ -0,0 +1,17 @@
+alert("+18 AUTHORIZED only");
+
+
+var age = prompt("how old are you?")
+
+if (age >= 18)
+{
+    alert("welcome to our page");
+}
+else
+{
+    alert("you may explore,not purchase")
+}
+
+
+
+
