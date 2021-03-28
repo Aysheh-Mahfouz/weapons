6cef94b1272378c9c80a7e87d32f93949794ce37
aysheh-ayoosh <ayoosh.dub@hotmail.com>
2021-03-28 21:31:15 +0300

    changes

--- app.js
+++ app.js
@@ -1,17 +1,48 @@
-alert("+18 AUTHORIZED only");
+alert("+18 AUTHORIZED only")
+
+
+
+
 
 
 var age = prompt("how old are you?")
-
-if (age >= 18)
-{
-    alert("welcome to our page");
-}
-else
-{
-    alert("you may explore,not purchase")
+while (age < 18);
+ {
+     age =prompt("how old are you?")
+    
+    
 }
 
+
+    
+
+
+
+var weaponType =prompt("your weapon of choise is!", "snipers , hunting rifles")
+ 
+while(weaponType !== "snipers" && weaponType !== "hunting rifles")
+{
+
+    weaponType =prompt("your weapon of choise is!", "snipers, hunting rifles")
+}
+
+
+
+var weaponNum =prompt('how many would you like to view?');
+var Image =(''); 
+
+for (var x  = 1; x <=weaponNum; x++)
+ {
+    if(weaponType === "hunting rifles")
+    {
+        image = image + '<img src="images\awm.webp" >';
+    } else if(weaponType === "snipers")
+     {
+        image = image + '<img src="images\huntingWep.png" ">';
+
+     } 
+    }
+    document.write('image');
 
 
 
