af46e3bcd79ef0ed72fb11138ae7b3554785545b
Aysheh Mahfouz <ayoosh.dub@hotmail.com>
2021-03-29 17:42:39 +0300

    functions add

--- app.js
+++ app.js
@@ -5,35 +5,48 @@ alert("+18 AUTHORIZED only")
 
 
 
-var age = prompt("how old are you?")
-while (age < 18);
+var year = prompt("year of birth?")
 
+function calAge() {
 
-var weaponType =prompt("your weapon of choise is!", "snipers , hunting rifles")
- 
-while(weaponType !== "snipers" && weaponType !== "hunting rifles")
-{
+    var result = 2021 - year
+    console.log(result)
 
-    weaponType =prompt("your weapon of choise is!", "snipers, hunting rifles")
+    return result
+}
+
+var result = calAge()
+
+while (result < 18) {
+    var year = prompt("year of birth?")
+    result = calAge()
+    console.log(result)
 }
 
 
 
-var weaponNum =prompt('how many would you like to view?');
-var image =''; 
 
-for (var x = 1; x <=weaponNum; x++)
-{
-    if(weaponType === 'hunting rifles')
-    {
-        image = image +  '<img src="packweps.png"/ >';
+var weaponType = prompt("your weapon of choise is!", "snipers , hunting rifles")
+
+while (weaponType !== "snipers" && weaponType !== "hunting rifles") {
+
+    weaponType = prompt("your weapon of choise is!", "snipers, hunting rifles")
+}
+
+
+
+var weaponNum = prompt('how many would you like to view?');
+var image = '';
+
+for (var x = 1; x <= weaponNum; x++) {
+    if (weaponType === 'hunting rifles') {
+        image = image + '<img src="packweps.png"/ >';
     }
-    else if(weaponType === 'snipers')
-    {
+    else if (weaponType === 'snipers') {
         image += '<img src="awm2.jpg"/ >';
-    } 
+    }
 }
-    document.write(image);
+document.write(image);
 
 
 
